c07b370309a2687ce20b825c2b91e96b345ae349
Kaapo.Studio <contact@kaapo.studio>
2022-03-05 15:00:31 +0200

    Auth environment fixed;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,6 @@ export const environment = {
     messagingSenderId: "269213411233",
     appId: "1:269213411233:web:4db52810dd5e9b23d50de0",
     measurementId: "G-WLRKZBQ1GK"
-  }
+  },
+  FIREBASE_API_KEY: 'AIzaSyAtFNJKfF4ATlWsOCjKMT-8MKm73qIbOO0',
 };
