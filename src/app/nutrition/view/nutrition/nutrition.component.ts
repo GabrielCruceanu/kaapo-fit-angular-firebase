c286c540d3afad6e3331fe30bbe1b3702bb1d7c7
Kaapo.Studio <contact@kaapo.studio>
2022-03-14 10:09:08 +0200

    paget titles fixed

--- src/app/nutrition/view/nutrition/nutrition.component.ts
+++ src/app/nutrition/view/nutrition/nutrition.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nutrition',
   templateUrl: './nutrition.component.html',
-  styleUrls: ['nutrition.component.scss'],
+  styleUrls: ['./nutrition.component.scss'],
 })
 export class NutritionComponent {}
