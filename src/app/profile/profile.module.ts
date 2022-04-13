defbc484701468f1d5fad47926c55091e170ca29
Kaapo.Studio <contact@kaapo.studio>
2022-04-13 11:08:57 +0300

    Swiper added;

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './view/profile/profile.component';
+import {SwiperModule} from "swiper/angular";
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [CommonModule, ProfileRoutingModule, SwiperModule],
 })
 export class ProfileModule {}
--- src/app/profile/view/profile/profile.component.ts
+++ src/app/profile/view/profile/profile.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {getUserDataMock, UserDetails} from "../../../../data/user.details";
+
+import SwiperCore, {EffectFade, Swiper} from 'swiper';
+
+SwiperCore.use([EffectFade]);
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.component.html',
   styleUrls: ['profile.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProfileComponent {
   title: string = 'My Profile';
