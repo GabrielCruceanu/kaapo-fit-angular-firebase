58a2e02b49dd98e460ea7cbf88e02ae1374b199d
Kaapo.Studio <71267761+kaapo-studio@users.noreply.github.com>
2022-05-10 16:29:39 +0300

    Menu fixed

--- src/app/shared/components/layout/layout.module.ts
+++ src/app/shared/components/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { ComponentsSharedModule } from '../components-shared.module';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, ComponentsSharedModule],
+  imports: [CommonModule, ComponentsSharedModule, MatBadgeModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
