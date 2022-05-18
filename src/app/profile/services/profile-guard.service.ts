270e66f33c1af270b5a3cf257a1258f56ebed768
Kaapo.Studio <71267761+kaapo-studio@users.noreply.github.com>
2022-05-18 08:34:52 +0300

    Fix profile-guard.service.ts

---
+++ src/app/profile/services/profile-guard.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileGuard implements CanActivate {
+  constructor(private store: Store<AppState>, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.router.createUrlTree(['profile/add']);
+  }
+}
