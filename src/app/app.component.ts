0c68a29440cc18034928a5683b9b6f82b4ccdee8
Kaapo.Studio <71267761+kaapo-studio@users.noreply.github.com>
2022-10-21 17:39:15 +0300

    Service worker added; Check update app added;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from './store/app.state';
 import { autoLogin } from './auth/store/auth.actions';
@@ -10,6 +10,8 @@ import {
   getTrainersStart,
   getUsersStart,
 } from '@/app/store/shared/shared.actions';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +19,14 @@ import {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store<AppState>) {}
+  constructor(
+    private store: Store<AppState>,
+    private update: SwUpdate,
+    private appRef: ApplicationRef
+  ) {
+    this.updateClient();
+    this.checkUpdate();
+  }
 
   ngOnInit() {
     this.store.dispatch(autoLogin());
@@ -27,5 +36,39 @@ export class AppComponent implements OnInit {
     this.store.dispatch(getTrainersStart());
     this.store.dispatch(getNutritionistsStart());
     this.store.dispatch(getReviewsStart());
+  }
+
+  updateClient() {
+    if (!this.update.isEnabled) {
+      console.log('Not Enable');
+      return;
+    }
+    this.update.available.subscribe((event) => {
+      console.log(`current`, event.current, `available `, event.available);
+      if (confirm('Modificari noi pentru aplicatie, confirma actualizarea.')) {
+        this.update.activateUpdate().then(() => location.reload());
+      }
+    });
+
+    this.update.activated.subscribe((event) => {
+      console.log(`current`, event.previous, `available `, event.current);
+    });
+  }
+
+  checkUpdate() {
+    this.appRef.isStable.subscribe((isStable) => {
+      if (isStable) {
+        const timeInterval = interval(8 * 60 * 60 * 1000);
+
+        timeInterval.subscribe(() => {
+          this.update
+            .checkForUpdate()
+            .then((isNewUpdate) =>
+              isNewUpdate ? this.updateClient() : console.log('checked')
+            );
+          console.log('update checked');
+        });
+      }
+    });
   }
 }
